998a8c5b2138ea1ebf7b5f95722fb2e40277ab30
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-07-30 16:30:26 +0200

    auto-generated getters and setters

--- src/banking/Account.java
+++ src/banking/Account.java
@@ -32,4 +32,37 @@ public class Account {
      */
     private double taux;
 
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Person getProprietaire() {
+		return proprietaire;
+	}
+
+	public void setProprietaire(Person proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+
+	public double getTaux() {
+		return taux;
+	}
+
+	public void setTaux(double taux) {
+		this.taux = taux;
+	}
+
+    
 }
--- src/banking/Person.java
+++ src/banking/Person.java
@@ -39,4 +39,46 @@ public class Person {
      */
     private PEL pel;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public MainAccount getCourant() {
+		return courant;
+	}
+
+	public void setCourant(MainAccount courant) {
+		this.courant = courant;
+	}
+
+	public PEL getPel() {
+		return pel;
+	}
+
+	public void setPel(PEL pel) {
+		this.pel = pel;
+	}
+    
+    
+
 }
